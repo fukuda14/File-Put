31904cb6f80f9f6a5e8a94a3af672bb8fcc69898
Gooner <kazoo@localhost.localdomain>
2013-06-25 07:15:22 -0700

    display progress

--- lib/efileput.pl
+++ lib/efileput.pl
@@ -26,7 +26,7 @@ GetOptions(\%opts,qw(number=i dir=s));
 my $number = $opts{'number'};
 my $dir = $opts{'dir'};
 
-# get the file
+# get the all file
 my @all_file = glob("$dir/*.cmd");
 my @files = ();
 
@@ -50,13 +50,21 @@ my $dircount = 1;
 my $filelist = [{}];
 pop @$filelist;
 
+my $max = @$list;
+my $now = 0;
+
 for my $i( 0 .. @$list -1 ) {
 
  push( @$filelist , { 'a' => $list->[$i]->{'a'} , 'b' => $list->[$i]->{'b'} , 'c' => $list->[$i]->{'c'} } );
 
   if( @$filelist >= $number ) {
+
       &copyfile( "./DATA$dircount", $filelist );
       $dircount++;
+
+      $now += @$filelist;
+      &progress( $now , $max , $number , $dircount );
+
       $filelist = [{}];
       pop @$filelist;
   }
@@ -66,7 +74,12 @@ for my $i( 0 .. @$list -1 ) {
 # default less than
 if( @$filelist > 0 ) {
    &copyfile( "./DATA$dircount", $filelist );
+
+   $now += @$filelist;
+   &progress( $now , $max , $number , $dircount );
 }
+
+print "\n";
 
 # file copy and list file
 sub copyfile {
@@ -74,18 +87,37 @@ sub copyfile {
   my ( $dir , $filelist ) = @_;
   my $newfile = "$dir/list";
 
- mkdir("$dir");
- open my $fh , '>' , $newfile;
+  mkdir("$dir");
 
- my @command = ();
- for my $i( 0 .. @$filelist-1 ) {
-   copy $filelist->[$i]->{'a'} , $dir;
-   copy $filelist->[$i]->{'b'} , $dir;
-   copy $filelist->[$i]->{'c'} , $dir;
-   push( @command , "$filelist->[$i]->{'a'} $filelist->[$i]->{'b'} $filelist->[$i]->{'c'}\n");
- }
+  open my $fh , '>' , $newfile;
 
- print $fh @command;
- close $fh;
+  my @command = ();
+  for my $i( 0 .. @$filelist-1 ) {
+    copy $filelist->[$i]->{'a'} , $dir;
+    copy $filelist->[$i]->{'b'} , $dir;
+    copy $filelist->[$i]->{'c'} , $dir;
+    push( @command , "$filelist->[$i]->{'a'} $filelist->[$i]->{'b'} $filelist->[$i]->{'c'}\n");
+  }
+
+  print $fh @command;
+
+  close $fh;
+
+}
+
+# display progress
+sub progress {
+
+  my ( $now , $max , $number , $dircount ) = @_;
+
+  sleep 0.8;
+
+  my $prog = $now * 100 / $max;
+  local $| = 1;
+  printf (" %3.0f%s" , $prog , '%:' );
+  my $i = $dircount * ( 20 / ( $max / $number ) ) ;
+  print "#" x $i;
+  print "\r";
+  local $| = 0;
 
 }
